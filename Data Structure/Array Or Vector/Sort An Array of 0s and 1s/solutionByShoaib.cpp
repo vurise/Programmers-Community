bcfda2049bb1e56ec7b6023f534f71a13ee0ae76
Shoaib Rayeen <shoaib.mca17.du@gmail.com>
2019-10-01 00:35:04 +0530

    by Shifting

---
+++ Data Structure/Array Or Vector/Sort An Array of 0s and 1s/solutionByShoaib.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+void sort01(int arr[], int n) {
+    int count = 0;
+    for (int i = 0; i < n; i++) {
+        if (arr[i] != 0) {
+            swap(arr[count++], arr[i]);
+        }
+    }
+}
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << "\n";
+}
+
+int main() {
+    int n;
+    cout << "\nEnter Size\t:\t";
+    cin >> n;
+    int *a = new int[n];
+    cout << "\nEnter Array Elements\n";
+    for ( int i = 0; i < n; i++ ) {
+        cin >> a[i];
+    }
+    sort01(a, n);
+    cout << "\nArray After Moving Zeros\t:\t";
+    printArray(a, n);
+    return 0;
+}
